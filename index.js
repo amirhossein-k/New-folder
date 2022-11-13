a27704f541bc4e69a63186c8891d1000968015be
amirhossein-k <amirhossein.k787878@gmail.com>
2022-11-13 06:43:14 -0800

    s

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express= require('express')
-const cors = require('cors')
+const express = require("express"); //for install => npm i express
 
-const app = express()
-app.use(cors())
 
-app.get('/api/user',(req,res)=>{
-    res.json({name: 'amir'})
-})
+
+
+const app = express();
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Listening on port ${8000}!`));
+
+app.get("/api/test", (req, res) => res.send({ message: "Hello" }));
+
+app.get("/api/notes", (req, res) => {
+  res.json({notes:'notes'});
+});
+
