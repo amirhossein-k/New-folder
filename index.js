4597008d6b60d513d5d44ed16d38d402b91e6a64
amirhossein-k <amirhossein.k787878@gmail.com>
2022-11-13 07:58:11 -0800

    45

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require("express"); //for install => npm i express
+const express = require("express");
+const cors = require('cors')
 
 
 
 
 const app = express();
+app.use(cors())
+
+app.use((req,res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Headers',"*");
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Listening on port ${8000}!`));
