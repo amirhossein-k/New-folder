dd548bd3af1300ffe115c41c8b268d26a6482361
amirhossein-k <amirhossein.k787878@gmail.com>
2022-11-13 08:52:57 -0800

    88

--- index.js
+++ index.js
@@ -7,17 +7,9 @@ const cors = require('cors')
 const app = express();
 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH,DELETE'); 
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true); 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({
+  origin: '*'
+}));
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Listening on port ${8000}!`));
