517e775cdb5c2b2c46fb7553eadefd6444bedb2e
amirhossein-k <amirhossein.k787878@gmail.com>
2022-11-13 07:29:19 -0800

    hh

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express"); //for install => npm i express
 const app = express();
 
 const PORT = 8000;
-app.listen(PORT, () => console.log(`Listening on port ${8000}!`));
+app.listen(porcces.env.PORT || PORT, () => console.log(`Listening on port ${8000}!`));
 
 app.get("/api/test", (req, res) => res.send({ message: "Hello" }));
 
